2473ab4ce79a9cbb728afa059d2b22272def52bf
lenondiedrich <lenon.diedrich@gmail.com>
2021-02-24 12:52:22 -0300

    Criação de rotas e refatoração do código

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { SurveysConstroller } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysConstroller();
 
 router.post("/users", userController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
