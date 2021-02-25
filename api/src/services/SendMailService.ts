4b7148fd9bccea11084e0b3aded70f5ace2f8896
lenondiedrich <lenon.diedrich@gmail.com>
2021-02-25 16:28:57 -0300

    Configuração inicial do envio de email

---
+++ api/src/services/SendMailService.ts
@@ -0,0 +1,36 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
+class SendMailService {
+    private client: Transporter;
+    constructor(){
+        nodemailer.createTestAccount()
+        .then(account => {
+            const transporter = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass
+                }
+            });
+
+            this.client = transporter;
+        });
+    }
+
+    async execute(to: string, subject: string, body: string){
+        
+        const message = await this.client.sendMail({
+            to,
+            subject,
+            html: body,
+            from: "NPS <noreply@nps.com.br>"
+        })
+
+        console.log("MEssage sent: %s", message.messageId);
+        console.log("PreviewURL: %s", nodemailer.getTestMessageUrl(message));
+    }
+}
+
+export default new SendMailService();
